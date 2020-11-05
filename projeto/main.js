f6a438d36c71ebbc05cdb73899bcf020ca379b73
lorenacjs <lorena-cjs@hotmail.com>
2020-11-05 18:17:49 -0300

    atualizando

--- projeto/main.js
+++ projeto/main.js
@@ -1,26 +1,92 @@
 var express = require("express");
 var app = express();
 
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+	host:"localhost",
+	user: "tecweb",
+	password: "tecweb",
+	database: "tecdespesaweb"
+});
+
 app.use(express.json());
+
+app.get("/despesa", (req, resp) => {
+	console.log("GET - Despesa");
+
+	connection.query("SELECT * FROM despesa", (err, result) => {
+		if (err) {
+  			console.log(err);
+  			resp.status(500).end();
+
+  		} else {
+  			resp.status(200);
+  			resp.json(result);
+  		}
+	});
+});
 
 app.post("/despesa", (req, resp) => {
   var despesa = req.body;
-  console.log(despesa);
+  console.log("POST - Despesa");
+
+  connection.query("INSERT INTO despesa SET ?", [despesa], (err, result) => {
+  	if (err) {
+  		console.log(err);
+  		resp.status(500).end();
+
+  	} else {
+  		resp.status(200);
+  		resp.json(result);
+  	}
+  }); 
 });
 
 app.get("/despesa/:despesaId", (req, resp) => {
   var despesaId = req.params.despesaId;
   console.log("GET - DespesaId: " + despesaId);
+
+  connection.query("SELECT * FROM despesa WHERE id = ?", [despesaId], (err, result) => {
+  	if (err) {
+  		console.log(err);
+  		resp.status(500).end();
+
+  	} else {
+  		resp.status(200);
+  		resp.json(result);
+  	}
+  });  
 });
 
 app.put("/despesa/:despesaId", (req, resp) => {
   var despesaId = req.params.despesaId;
+  var despesa = req.body();
   console.log("PUT - DespesaId: " + despesaId);
+
+  connection.query("UPDATE despesa SET ? WHERE id = ?", [despesa, despesaId], (err, result) => {
+  	if (err) {
+  		console.log(err);
+  		resp.status(500).end();
+
+  	} else {
+  		resp.status(200).end();
+  	}
+  });
 });
 
 app.delete("/despesa/:despesaId", (req,resp) => {
   var despesaId = req.params.despesaId;
   console.log("DELETE - DespesaId: " + despesaId);
+
+  connection.query("DELETE FROM despesa WHERE id = ?", [despesaId], (err, result) => {
+  	if (err) {
+  		console.log(err);
+  		resp.status(500).end();
+
+  	} else {
+  		resp.status(200).end();
+  	}
+  });
 });
 
 app.listen(3000, () => {
